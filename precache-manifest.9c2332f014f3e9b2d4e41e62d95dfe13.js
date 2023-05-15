df0d43b55407cb49ec6f88620d1c60aa2511570b
Aakash <itshappinesslife@gmail.com>
2023-05-15 11:45:53 +0530

    Updates

---
+++ precache-manifest.9c2332f014f3e9b2d4e41e62d95dfe13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4aa21fd80147af0cd902",
+    "url": "/WordAnalyzer/static/css/main.df86dae7.chunk.css"
+  },
+  {
+    "revision": "4aa21fd80147af0cd902",
+    "url": "/WordAnalyzer/static/js/main.4aa21fd8.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/WordAnalyzer/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/WordAnalyzer/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "ba387797ea86daffbb34",
+    "url": "/WordAnalyzer/static/js/runtime~main.ba387797.js"
+  },
+  {
+    "revision": "427fb909e277ff0b6ce0240321d672f5",
+    "url": "/WordAnalyzer/index.html"
+  }
+];
